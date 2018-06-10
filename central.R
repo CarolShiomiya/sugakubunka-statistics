37f5b4aaa0f530579144e68c07266b7b954989b1
Akihiko Sato <a_sato@bio.c.u-tokyo.ac.jp>
2018-06-10 12:14:09 +0900

    18/06/10

--- central.R
+++ central.R
@@ -1,5 +1,10 @@
+#中心極限定理を確認しよう。
+
 #母集団は平均30 分散25　の正規分布
-#n=10,100,1000,10000,100000,1000000のサンプルをとる。
+#n=10,100,1000,10000,100000のサンプルをそれぞれ1000回抽出する。
+#当たり前だが1000回標本抽出すると、標本平均は1000個できる。
+#標本平均の分布をヒストグラムにした。
+par(mfrow = c(2,3))
 
 for (j in 1:5){
   size=10^j
@@ -18,11 +23,11 @@ for (j in 1:5){
   mean(sampleave)
   mean(samplevar)
   mean(samplevar2)
-
-  hist(sampleave)
+  name<-paste("sample size=",10^j)
+  hist(sampleave,seq(20,40,1/sqrt(10^(j-1))),main=name)
+  legend("topright",paste("sd=",round(sd(sampleave),3)))
   #正規分布を決めるのは平均（期待値）とsd
   mean(sampleave)
-  print(sd(sampleave))
-  #hoge<-function(x){10000*dnorm(x,mean(sampleave),sd(sampleave))}
-  #curve(hoge,from=26,to=34,add=TRUE)
+  hoge<-function(x){1000*(1/sqrt(10^(j-1)))*dnorm(x,mean(sampleave),sd(sampleave))}
+  curve(hoge,from=26,to=34,add=TRUE,col="red") #正規分布のグラフを重ねて描く
 }
