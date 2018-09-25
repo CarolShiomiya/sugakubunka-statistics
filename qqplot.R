d4ad87565ae3ff5386068cbb90bb74137ecb58c3
CarolShiomiya <shiosainouta_carol@hotmail.co.jp>
2018-09-25 19:41:24 +0900

    qqplot

--- qqplot.R
+++ qqplot.R
@@ -1,15 +1,18 @@
 a<-1:34
+a
 a2<-a/35
-x<-qnorm(a2)
-y=1:34
-plot(x,y)
+a2
+x<-qnorm(a2) #正規分布していた場合の、ｚ得点（低い順に並べた）
+x
+y<-scale(1:34) #本当の得点を低い順番に並べたものをz得点化
+plot(x,y) #本当の得点が正規分布に近ければ、y=xの直線上に並ぶはずだ。 
 
 y<-rnorm(34,0,1)
 y2<-sort(y)
 plot(x,y2)
 
 y<-runif(34,-1,1)
-y3<-sort(y)
+y3<-scale(sort(y))
 plot(x,y3)
 hoge<-function(x){y=x}
 curve(hoge,add=TRUE)
