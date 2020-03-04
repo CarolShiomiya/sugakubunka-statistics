2a92d421569ceaca7a1390700c16a4eb75e62b16
CarolShiomiya <shiosainouta_carol@hotmail.co.jp>
2020-03-04 16:30:05 +0900

    sigmoid

--- bayse/rstan_threshold.R
+++ bayse/rstan_threshold.R
@@ -1,9 +1,9 @@
 library(rstan)
 
 #1.データ作成
-x=c(rep(1,50),rep(-1,50))
+x=c(rep(1,100),rep(-1,70))
 x
-y=round(c(runif(50,1960,2010),runif(50,1900,1970)))
+y=round(c(runif(100,1960,2010),runif(70,1900,1970)))
 df=data.frame(y=y,x=x)
 df
 plot(x,y)
@@ -16,20 +16,20 @@ real sigmoiddist_log(real x,real a, real b){
   real temp;
   real bunbo;
   temp = inv_logit(a*(x-b));
-  bunbo =log((1/a)*( log(1+exp(a*(2020-b))) - log(1+exp(a*(1800-b))) ));
+  bunbo =log((1/a)*( log(1+exp(a*(2100-b))) - log(1+exp(a*(1800-b))) ));
   return temp - bunbo;
   }
 }
 data{
-  int Takei[100];
-  int Y[100]; 
+  int Takei[170];
+  int Y[170]; 
 }
 parameters {
 real<lower = -1, upper=1> alpha;
 real<lower=1900,upper=1990> beta;
 }
 model{
-for (i in 1:40){
+for (i in 1:170){
   Y[i]~sigmoiddist(Takei[i]*alpha,beta) ;
 }
 }"
@@ -49,5 +49,7 @@ traceplot(fit)
 stan_hist(fit)
 stan_hist(fit,pars=c("alpha","beta"))
 
-ms=extract(fit)
-ms$yhat
+#今度は年代を使って、多形があるかどうか予測するロジスティック回帰モデルを作成し、
+#年代の回帰係数が大きいものを絞り込む。
+library(brms)
+fit_brm <- brm(x ~ y , data=df, family="bernoulli",iter=2000,warmup=1000,chain=2)
